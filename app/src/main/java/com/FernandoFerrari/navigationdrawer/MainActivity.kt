f8c09696dd700bfafcab75b6115b1571a21f9527
FernandocFeijao <fernandofkz@hotmail.com>
2026-09-18 10:21:58 -0300

    Atividade realizada

---
+++ app/src/main/java/com/FernandoFerrari/navigationdrawer/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.example.navigationdrawer
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.addCallback
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.navigationdrawer.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Hide the status bar.
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).hide(WindowInsetsCompat.Type.statusBars())
+
+        setSupportActionBar(binding.toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolbar,
+            R.string.nav_open,
+            R.string.nav_close
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_cart -> openFragment(CartFragment())
+                R.id.bottom_profile -> openFragment(ProfileFragment())
+                R.id.bottom_menu -> openFragment(MenuFragment())
+            }
+            true
+        }
+
+        fragmentManager = supportFragmentManager
+        if (savedInstanceState == null) {
+            openFragment(HomeFragment())
+        }
+
+        binding.fab.setOnClickListener {
+            Toast.makeText(this, "Categorias", Toast.LENGTH_SHORT).show()
+        }
+
+        onBackPressedDispatcher.addCallback(this) {
+            if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                finish()
+            }
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_books -> openFragment(LivrosFragment())
+            R.id.nav_remedios -> openFragment(RemediosFragment())
+            R.id.nav_maletas -> openFragment(MaletasFragment())
+            R.id.nav_bikes -> Toast.makeText(this, "Bikes", Toast.LENGTH_SHORT).show()
+            R.id.nav_trofeus -> Toast.makeText(this, "Trofeus", Toast.LENGTH_SHORT).show()
+            R.id.nav_relogios -> Toast.makeText(this, "Relógios", Toast.LENGTH_SHORT).show()
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+    }
+}
---
+++ app/src/main/java/com/FernandoFerrari/navigationdrawer/RemediosFragment.kt
@@ -0,0 +1,16 @@
+package com.example.navigationdrawer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class RemediosFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_remedios, container, false)
+    }
+}
